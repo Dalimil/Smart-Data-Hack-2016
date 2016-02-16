11bee17d700c0a0b80ffbc67fb08c51d70172216
Dalimil <dalimilhajek@gmail.com>
2016-02-16 16:28:42 +0000

    Change from OSM to GeoJSON

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 var data = null;
-var original_data = null;
 var timestamps = null;
+var currentTime = null;
 var layer = null;
 var map = null;
 
 function update(time_id){
-	var usedT = timestamps.slice(0, time_id+1);
-	data.children[0] = rebuild_xml(original_data.children[0], usedT);
+	currentTime = timestamps[time_id];
+	console.log("currentTime: "+currentTime);
 	display();
 }
+
+function fromTimestamp(str){
+    return new Date(str).getTime();   
+}
+
+var geojsonMarkerOptions = {
+    radius: 6,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
 
 function display(){
 	if(layer != null){
 		map.removeLayer(layer);
 	}
 
+  	layer = L.geoJson(data, {
+		pointToLayer: function (feature, latlng) {
+	      	return L.circleMarker(latlng, geojsonMarkerOptions);
+		},
+		filter: function(feature, layer) {
+			if(feature.geometry.type == 'Point') return false; // todo?
+		    return (feature.properties.meta.timestamp.substr(0, 8) <= currentTime.substr(0,8));
+	    }
+	});
+  	layer.setStyle({color: 'black', fillColor: 'blue', fillOpacity: 0.5 });
+  	layer.addTo(map);
+  	map.fitBounds(layer.getBounds());
+
+  	/*
 	layer = new L.OSM.DataLayer(data)
     layer.setStyle({color: 'black', fillColor: 'orange', fillOpacity: 0.5 });
     layer.addTo(map);
-
     map.fitBounds(layer.getBounds());
+    */
 }
 
 $( document ).ready(function(){
@@ -42,79 +69,42 @@ $( document ).ready(function(){
 	  url: "data.osm",
 	  dataType: "xml",
 	  success: function (xml) {
-	 	console.log(xml);
-	 	timestamps = getUniqueTimestamps(xml);
+	 	data = osmtogeojson(xml);
+	 	console.log(data);
+	 	removePoints();
+	 	console.log(data);
+	 	timestamps = getUniqueTimestamps(data);
 	 	console.log(timestamps);
-	 	data = xml;
-	 	original_data = xml;
-	 	display();
+	 	update(timestamps.length-1);
 	  }
 	});
 
 	
 });
 
-function rebuild_xml(g, usedT){
-	var z = g.getAttribute("timestamp");
-	if(z != null){
-		z = z.substr(0, 8);
-		var consider = false;
-		for(var i = 0;i<usedT.length;i++){
-			if(usedT[i].substr(0, 8) == z){
-				consider = true;
-				break;
-			}
+function removePoints(){
+	for(var i=data.features.length-1;i>=0;i--){
+		if(data.features[i].geometry.type == 'Point'){
+			data.features.splice(i, 1);
 		}
-		if(consider){
-			return g;
-		}else{
-			return null;
-		}
-	}else{ // no timestamp
-		var g2 = g;
-		g2.children = [];
-		for(var i=0;i<g.children.length;i++){
-			var c = g.children[i];
-			var k = rebuild_xml(c, usedT);
-			if(k != null){
-				g2.children.push(k);
-			}
-		}
-		return g2;
 	}
 }
 
+function getUniqueTimestamps(data){
+	var ts = [];
+	for(var i=0;i<data.features.length;i++){
+		if(data.features[i].geometry.type != 'Point'){
+			ts.push(data.features[i].properties.meta.timestamp);
+		}
+	}
 
-function getUniqueTimestamps(xml){
-	var timestamps = getTimestamps(xml.children[0]).split(";").sort();
- 	for(var i=timestamps.length-2;i>=0;i--){
- 		var a = timestamps[i+1].substr(0, 8);
- 		var b = timestamps[i].substr(0, 8);
+	ts = ts.sort();
+ 	for(var i=ts.length-2;i>=0;i--){
+ 		var a = ts[i+1].substr(0, 8);
+ 		var b = ts[i].substr(0, 8);
  		if(a == b){
- 			timestamps.splice(i+1, 1);
+ 			ts.splice(i+1, 1);
  		}
  	}
- 	return timestamps;
-}
-
-function getTimestamps(g){
-	var ans = "";
-	for(var i=0;i<g.children.length;i++){
-		var c = g.children[i];
-		var k = getTimestamps(c);
-		if(k.length > 0){
-			if(ans.length > 0){
-				ans += ";";
-			}
-			ans += k;
-		}
-	}
-	var z = g.getAttribute("timestamp");
-	if(z != null){
-		if(ans.length > 0){
-			ans += ";";
-		}
-		ans += z;
-	}
-	return ans;
+ 	return ts;
 }
