fb4b973f690f1a4c305bc5a7ad4c7f9df45d4fc5
Dalimil <dalimilhajek@gmail.com>
2016-02-16 14:42:59 +0000

    Getting timestamps

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-	var map = L.map('map').setView([51.505, -0.09], 1);
+	var map = L.map('map').setView([50, 0], 3);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGhoaGhoaGhoaGgiLCJhIjoiY2lrcGRrenhrMDBhaXc4bHMwNXd3emszbiJ9.GSEKdLMRDLkp5HJozOsw_g', {
 		maxZoom: 18,
@@ -9,20 +9,60 @@ $( document ).ready(function(){
 		id: 'mapbox.streets'
 	}).addTo(map);
 
+	var sw = map.getBounds()._southWest;
+	var ne = map.getBounds()._northEast;
+
 	//new L.OSM.Mapnik().addTo(map);
 	$.ajax({
+	  //url: "http://api.openstreetmap.org/api/0.6/map?bbox="+sw.lon+","+sw.lat+","+ne.lon+","+ne.lat;
 	  url: "data.osm",
-	  // or "http://www.openstreetmap.org/api/0.6/way/52477381/full"
 	  dataType: "xml",
 	  success: function (xml) {
+	 	console.log(xml);
+	 	var timestamps = getUniqueTimestamps(xml);
+
+	 	console.log(timestamps);
+
 	    var layer = new L.OSM.DataLayer(xml)
 	    layer.setStyle({color: 'black', fillColor: 'orange', fillOpacity: 0.5 });
 	    layer.addTo(map);
+
 	    map.fitBounds(layer.getBounds());
 	  }
 	});
+
+	function getUniqueTimestamps(g){
+		var timestamps = getTimestamps(xml.children[0]).split(";").sort();
+	 	for(var i=timestamps.length-2;i>=0;i--){
+	 		var a = timestamps[i+1].substr(0, 8);
+	 		var b = timestamps[i].substr(0, 8);
+	 		if(a == b){
+	 			timestamps.splice(i+1, 1);
+	 		}
+	 	}
+	 	return timestamps;
+	}
+
+	function getTimestamps(g){
+		var ans = "";
+		for(var i=0;i<g.children.length;i++){
+			var c = g.children[i];
+			var k = getTimestamps(c);
+			if(k.length > 0){
+				if(ans.length > 0){
+					ans += ";";
+				}
+				ans += k;
+			}
+		}
+		var z = g.getAttribute("timestamp");
+		if(z != null){
+			if(ans.length > 0){
+				ans += ";";
+			}
+			ans += z;
+		}
+		return ans;
+	}
 });
-
-
-
 
