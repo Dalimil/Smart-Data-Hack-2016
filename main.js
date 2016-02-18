947cdb3705f1a8249a37a6acc955d9e773caceb7
Dalimil <dalimilhajek@gmail.com>
2016-02-18 13:11:47 +0000

    Fix stretch

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var compareLen = 10;
 var sliderCreated = false;
 
 function initIntro(){
-	$("#intro").css("width", $(document).width()); 
-	$("#intro").css("height", $(document).height()); 
+	//$("#intro").css("width", $(document).width()); 
+	//$("#intro").css("height", $(document).height()); 
 	$("#intro").show();
 	$("#main").hide();
 }
