14752ad816116cffd7f6544b8381e4e67c64d8d5
Aidenkwok <aidenkwok96@gmail.com>
2016-02-18 17:05:09 +0000

    Humanitarian Map Tile

--- main.js
+++ main.js
@@ -197,7 +197,7 @@ $( document ).ready(function(){
 	}).addTo(map);*/
 
 
-L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
