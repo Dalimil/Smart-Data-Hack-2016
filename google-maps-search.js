376d0eeffaf71bf998919d24237996001cf016c6
Dalimil <dalimilhajek@gmail.com>
2016-02-18 13:59:34 +0000

    Add search functionality for intro page

--- google-maps-search.js
+++ google-maps-search.js
@@ -2,9 +2,18 @@
 google.maps.event.addDomListener(window, 'load', intilize);
 function intilize() {
     var ac = new google.maps.places.Autocomplete(document.getElementById("txtautocomplete"));
+    var ac2 = new google.maps.places.Autocomplete(document.getElementById("txtautocomplete2"));
+
     google.maps.event.addListener(ac, 'place_changed', function () {
         var place = ac.getPlace();
         console.log(place.geometry.location.lat() +", "+place.geometry.location.lng());
         map.setView([place.geometry.location.lat(), place.geometry.location.lng()], 13);
+    });
+
+    google.maps.event.addListener(ac2, 'place_changed', function () {
+        var place = ac2.getPlace();
+        console.log(place.geometry.location.lat() +", "+place.geometry.location.lng());
+        map.setView([place.geometry.location.lat(), place.geometry.location.lng()], 13);
+        skipIntro();
     });
 };
